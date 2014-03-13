09643371fbe84a3bbcbd628e3cfc58c1203d8358
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-03-13 13:56:02 -0600

    Added a new item

--- lib/menu.rb
+++ lib/menu.rb
@@ -5,7 +5,8 @@ class Menu
   def items
     [
       Item.new("Channa Masala", 5.95, "Yummy goodness"),
-      Item.new("Chicken Tikka Masala", 5.95, "Yummy goodness")
+      Item.new("Chicken Tikka Masala", 5.95, "Yummy goodness"),
+      Item.new("Saag Paneer",5.95,"Yummy goodness")
     ]
   end
 
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -8,7 +8,8 @@ describe "The menu" do
 
     expect(menu.items).to match_array([
                                Item.new("Channa Masala", 5.95, "Yummy goodness"),
-                               Item.new("Chicken Tikka Masala", 5.95, "Yummy goodness")
+                               Item.new("Chicken Tikka Masala", 5.95, "Yummy goodness"),
+                               Item.new("Saag Paneer",5.95,"Yummy goodness")
                              ])
   end
 end
